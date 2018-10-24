5d73eba5262c72959640a2776343ba0d6df6967e
donluispanis <luisgonzalezaracil@hotmail.com>
2018-10-24 21:32:35 +0200

    Ugly, but works

--- src/Fire/FireDemo.cpp
+++ src/Fire/FireDemo.cpp
@@ -7,10 +7,9 @@ bool FireDemo::Init()
     width = GetWidth();
     height = GetHeight();
     screenMapping = new unsigned char[width * height];
-    colourMap = new Pixel[256]; 
+    colourMap = new Pixel[256];
 
-    for(int i = 0; i < 256; i++)
-        colourMap[i].R = (unsigned char)i;
+    createColourMap({ColourStamp{0, Pixel{0,0,0}}, ColourStamp{0, Pixel{255,255,255}}});
 
     return true;
 }
@@ -23,9 +22,8 @@ bool FireDemo::Update(float fDeltaTime)
 
         if (fast_rand() % 2 != 0)
             screenMapping[i] = 255;
-        
-        pixels[i] = Pixel(255,0,0); //fast equivalent to -> pixels[i] = colourMap[screenMapping[i]];
-        
+
+        pixels[i] = Pixel(255, 0, 0); //fast equivalent to -> pixels[i] = colourMap[screenMapping[i]];
     }
 
     for (int i = width * (height - 1); i >= 0; i--)
@@ -36,7 +34,7 @@ bool FireDemo::Update(float fDeltaTime)
     }
     return true;
 }
-  
+
 bool FireDemo::Destroy()
 {
     delete[] screenMapping;
@@ -45,9 +43,40 @@ bool FireDemo::Destroy()
     return true;
 }
 
+void FireDemo::createColourMap(std::vector<ColourStamp> colours)
+{
+    if (colours.size() == 0)
+        ShowError("FireDemo::createColourMap => No colour map was provided");
+
+    if (colours.size() == 1)
+        for (int i = 0; i < 256; i++)
+            colourMap[i] = colours[0].colour;
+
+    if (colours.size() > 1)
+    {
+        char from, to, counter;
+        from = 0;
+        counter = 1;
+        to = colours[1].percentage * 255;
+
+        float t = 1.f, tprima = 0.f;
+
+        for (int i = 0; i < 256; i++)
+        {
+            
+
+            t = (float)(255 - i) / 255;
+            tprima = 1 - t;
+            colourMap[i].R = colours[0].colour.R * t + colours[1].colour.R * tprima;
+            colourMap[i].G = colours[0].colour.G * t + colours[1].colour.G * tprima;
+            colourMap[i].B = colours[0].colour.B * t + colours[1].colour.B * tprima;
+        }
+    }
+}
+
 unsigned long FireDemo::fast_rand() //TODO := Create my own library (?)
 {
-    static unsigned long x=123456789, y=362436069, z=521288629;
+    static unsigned long x = 123456789, y = 362436069, z = 521288629;
     static unsigned long t;
     x ^= x << 16;
     x ^= x >> 5;
--- src/Fire/FireDemo.h
+++ src/Fire/FireDemo.h
@@ -1,4 +1,8 @@
+#pragma once
+
 #include "../ClassicDemoTemplate.h"
+
+#include <vector>
 
 class FireDemo : public ClassicDemoTemplate
 {
@@ -11,6 +15,24 @@ class FireDemo : public ClassicDemoTemplate
     virtual bool Init() override;
     virtual bool Update(float fDeltaTime) override;
     virtual bool Destroy() override;
+
+    struct ColourStamp {
+      float percentage;
+      Pixel colour;
+
+      ColourStamp(float perc, Pixel col) {
+        if(perc > 1.0f)
+          percentage = 1.0f;
+        else if(perc < 0.0f)
+          percentage = 0.0f;
+        else
+          percentage = perc;
+
+        colour = col;
+      }
+    };
+
+    void createColourMap(std::vector<ColourStamp> colours);
 
     Pixel* pixels;
     int width, height;
