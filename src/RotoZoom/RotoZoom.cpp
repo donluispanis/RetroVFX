656d50e896b87a27899fcf8d6a5fc851a086c88d
donluispanis <luisgonzalezaracil@hotmail.com>
2019-02-05 20:27:13 +0100

    Partially working

--- src/RotoZoom/RotoZoom.cpp
+++ src/RotoZoom/RotoZoom.cpp
@@ -17,7 +17,7 @@ bool RotoZoom::Init()
     Fast::GenerateSineTable(sineTable, mathTableSize);
     cosineTable = new float[mathTableSize];
     Fast::GenerateCosineTable(cosineTable, mathTableSize);
-    
+
     BMP::OpenRGBImage("assets/img/lena.bmp", texture, texWidth, texHeight);
 
     return true;
@@ -25,25 +25,40 @@ bool RotoZoom::Init()
 
 bool RotoZoom::Update(float deltaTime)
 {
+    static int offsetX = 0;
+    static int offsetY = 0;
+    static int angle = 0;
+    static float scale = 1;
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
         {
-            DrawPixel(i, j, 0, 0, 0, 2.f);
+
+            DrawPixel(i, j, offsetX, offsetY, angle, scale);
         }
     }
-    
+    offsetX++;
+    offsetY++;
+    angle++;
+    angle++;
+    angle++;
+    angle++;
+    scale += 0.1;
+
     RenderText("Press space to change effect", 5, 5, 2, Pixel{255, 255, 255});
     return true;
 }
 
 void RotoZoom::DrawPixel(int x, int y, int offsetX, int offsetY, int angle, float scale)
 {
-    int texX = int((x + offsetX)  * scale) % texWidth; 
-    int texY = int((y + offsetY) * scale) % texHeight; 
+    float sine = sineTable[angle % mathTableSize];
+    float cosine = cosineTable[angle % mathTableSize];
 
-    /** cosineTable[angle % mathTableSize]
-* sineTable[angle % mathTableSize] **/
+    int texX = (x + offsetX) / scale;
+    int texY = (y + offsetY) / scale;
+
+    texX = int(x * cosine - y * sine) % texWidth;
+    texY = int(y * cosine + x * sine) % texHeight;
 
     pixels[y * width + x] = texture[texY * texWidth + texX];
 }
@@ -52,8 +67,8 @@ bool RotoZoom::Destroy()
 {
     BMP::CloseRGBImage(texture);
 
-    delete [] sineTable;
-    delete [] cosineTable;
+    delete[] sineTable;
+    delete[] cosineTable;
 
     return true;
 }
