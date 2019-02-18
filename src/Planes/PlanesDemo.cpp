5fee329eb208f513ecb7e2809b7c16344a203dbf
donluispanis <luisgonzalezaracil@hotmail.com>
2019-02-18 20:15:02 +0100

    Mode 7 kind of working

--- src/Planes/PlanesDemo.cpp
+++ src/Planes/PlanesDemo.cpp
@@ -25,16 +25,19 @@ bool PlanesDemo::Init()
     nearPlane = 0.01f;
     farPlane = 0.5f;
 
-    texture = new Pixel[256 * 256];
-    texWidth = 256;
-    texHeight = 256;
+    texture = new Pixel[80 * 80];
+    texWidth = 80;
+    texHeight = 80;
     texSize = texWidth * texHeight;
 
     for (int i = 0; i < texWidth; i++)
     {
         for (int j = 0; j < texHeight; j++)
         {
-            //texture[j * texWidth + i] = Pixel(255) * noiseMap[j * texWidth + i];
+            if (i % 20 == 0 || j % 20 == 0 || i % 20 == 1 || j % 20 == 1 || i % 20 == 2 || j % 20 == 2 || i % 20 == 3 || j % 20 == 3)
+            {
+                texture[j * texWidth + i] = Pixel(255);
+            }
         }
     }
 
@@ -62,12 +65,48 @@ bool PlanesDemo::Update(float deltaTime)
 {
     UpdateInput(deltaTime);
 
-    for (int i = 0; i < width; i++)
+    int pixelScaleFactor = 4;
+
+    int maxWidth = width / pixelScaleFactor;
+    int maxHeight = height / pixelScaleFactor;
+    int startingWidth = -maxWidth / 2;
+    int startingHeight = maxHeight / 2;
+
+    double _x, _y;
+
+    //z - the incrementable variable that beggins at -300 and go to 300, because
+    //the depth will be in the center of the HEIGHT
+    double z = (height / 2) * -1;
+
+    //Scales just to control de scale of the printed pixel. It is not necessary
+    double scaleX = 16.0;
+    double scaleY = 16.0;
+
+    //Mode 7 - loop (Left Top to Down)
+    for (int y = 0; y < height; y++)
     {
-        for (int j = 0; j < height; j++)
+
+        _y = y / (z == 0 ? 1 : z); //The new _y coord generated
+        if (_y < 0)
+            _y *= -1;              //Control the _y because the z starting with a negative number
+        _y *= scaleY;              //Increase the size using scale
+        _y = fmod(_y, texHeight); //Repeat the pixel avoiding get texture out of bounds
+
+        for (int x = 0; x < width; x++)
         {
-            pixels[j * width + i] = texture[(j * texWidth + i) % texSize];
+
+            _x = (width / 2 - x) / (z == 0 ? 1 : z); //The new _x coord generated
+            if (_x < 0)
+                _x *= -1;             //Control the _x to dont be negative
+            _x *= scaleX;             //Increase the size using scale
+            _x = fmod(_x, texWidth); //Repeat the pixel avoiding get texture out of bounds
+
+            //Set x,y of the view image with the _x,_y pixel in the texture
+            pixels[y * width + x] = texture[Fast::Abs((int)_y * texWidth + (int)_x)];
         }
+
+        //Increment depth
+        z++;
     }
 
     RenderText("dfsgdftgh", 5, 5, 2, Pixel(255));
@@ -94,7 +133,8 @@ void PlanesDemo::UpdateInput(float deltaTime)
 
     if (goForth)
     {
-        cameraPosition.Y += deltaTime * 200;
+        cameraPosition.X = cameraPosition.X * cosf(cameraAngle) - cameraPosition.Y * sinf(cameraAngle);
+        cameraPosition.Y = cameraPosition.X * sinf(cameraAngle) + cameraPosition.Y * cosf(cameraAngle);
     }
     if (goBack)
     {
